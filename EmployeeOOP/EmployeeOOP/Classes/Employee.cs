f5d721c8e0b69228e401e0c9db5565ad4e159630
SaraCGil43 <saritacolgil23@gmail.com>
2023-09-19 20:11:51 -0500

    Class ComissionEmployee

--- EmployeeOOP/EmployeeOOP/Classes/Employee.cs
+++ EmployeeOOP/EmployeeOOP/Classes/Employee.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace EmployeeOOP.Classes
 {
-    internal class Employee
+    public abstract class Employee
     {
+        #region properties
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public Date? BirthDate { get; set; }
+        public Date? HiringDate { get; set; }
+        public bool? IsActive { get; set; }
+        #endregion
+
+        #region Methods
+        public Employee()
+        {
+
+        }
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"Información empleado \n\t " +
+                $"ID: {Id}\n\t" +
+                $"Nombres: {FirstName}\n\t" +
+                $"Apellidos: {LastName}\n\t" +
+                $"Fecha de Cumpleaños: {BirthDate}\n\t" +
+                $"Fecha contratación: {HiringDate}\n\t" +
+                $"¿Empleado activo: {IsActive}\n\t";
+        }
+
+
+        #endregion
     }
 }
