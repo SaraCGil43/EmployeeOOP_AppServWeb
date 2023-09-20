de67160bacb11ee7ab3e48a5885d39c652940cda
SaraCGil43 <saritacolgil23@gmail.com>
2023-09-19 20:20:24 -0500

    Class SalaryEmployee

--- EmployeeOOP/EmployeeOOP/Classes/HourtyEmployee.cs
+++ EmployeeOOP/EmployeeOOP/Classes/HourtyEmployee.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace EmployeeOOP.Classes
 {
-    internal class HourtyEmployee
+    public class HourtyEmployee : Employee
     {
+        #region Properties
+
+        public float Hours { get; set; }
+        public decimal HourValue { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public HourtyEmployee()
+        {
+
+        }
+
+        public override decimal GetValueToPay()
+        {
+            
+            return HourValue * (decimal)Hours;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                   $"Horas Trabajadas: {Hours:F2}\n" +
+                   $"Valor por Hora: {HourValue:C2}\n" +
+                   $"Total valor: {GetValueToPay():C2}\n\t";
+        }
+
+        #endregion
     }
 }
